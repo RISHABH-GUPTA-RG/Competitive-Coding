1ee90c02328f47dea6a9b94a88af86c02a2e02cd
Rishabh <rishabhguptarg.alld@gmail.com>
2024-10-07 15:10:45 +0530

    changes

---
+++ B_Fix_You.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        char arr[n][m];
+
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                cin >> arr[i][j];
+            }
+        }
+    }
+}
